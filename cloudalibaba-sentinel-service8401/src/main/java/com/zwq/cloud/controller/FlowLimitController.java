bb80f208bc33c317acb16cc2857eddf9e0c1f3a0
wuqing.zhu <wuqing.zhu@envision-energy.com>
2021-03-17 09:57:28 +0800

    8401进行调整

--- cloudalibaba-sentinel-service8401/src/main/java/com/zwq/cloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/zwq/cloud/controller/FlowLimitController.java
@@ -1,9 +1,11 @@
 package com.zwq.cloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("testA")
@@ -23,7 +25,20 @@ public class FlowLimitController {
 
     @GetMapping("testB")
     public String getTestB(){
-
+        System.out.println("调用B");
+        log.info(Thread.currentThread().getName()+" \t\t testB");
         return  "Test B.......";
     }
+
+    @GetMapping("/testD")
+    public String testD()
+    {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {e.printStackTrace();}
+        log.info(Thread.currentThread().getName()+"\t"+"...testD 测试RT");
+        return "------testD 测试RT";
+    }
+
+
 }
