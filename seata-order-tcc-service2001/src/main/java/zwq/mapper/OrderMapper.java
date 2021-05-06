504eefd6390854c9f1c09d9d6ebe71a4e6843ddc
wuqing.zhu <wuqing.zhu@envision-energy.com>
2021-05-06 16:48:15 +0800

    删除一些无用的项目

--- seata-order-tcc-service2001/src/main/java/zwq/mapper/OrderMapper.java
+++
@@ -1,9 +0,0 @@
-package zwq.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import zwq.domain.Order;
-
-@Mapper
-public interface OrderMapper extends BaseMapper<Order> {
-}
